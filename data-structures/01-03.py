4cc76083889b4dc7ec785c26faf5bc768bc10280
Vladimír Zdražil <mail@vzdrazil.com>
2016-11-08 16:51:43 +0100

    Reverse a C-Style string, remove duplicate characters in a string

---
+++ data-structures/01-03.py
@@ -0,0 +1,15 @@
+# Design an algorithm and write code to remove duplicate characters in a string without using any additional buffer. NOTE: one or two variables are fine. An extra copy of an array is not.
+
+string = 'abfcdfe2'
+
+def remove_dup(input):
+    string_array = list(input)
+    for ind, val in enumerate(string_array):
+        for j_ind, j_val in enumerate(string_array):
+            if (ind != j_ind) and (val == j_val) and j_val != '':
+                string_array[j_ind] = ''
+    
+    return ''.join(string_array)
+
+print(string)
+print(remove_dup(string))
