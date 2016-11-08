4bbefb7ec184b7803f8c3186f4a4d551fd01c6f7
Vladimír Zdražil <mail@vzdrazil.com>
2016-11-08 17:03:43 +0100

    Decide if anagram

---
+++ data-structures/01-04.py
@@ -0,0 +1,20 @@
+# Write a method to decide if two string are anagrams or not
+
+string1 = 'William Shakespeare'
+string2 = 'I am a weakish speller'
+
+def is_anagram(string1, string2):
+    string1 = string1.replace(" ", "")
+    string2 = string2.replace(" ", "")
+    string1 = string1.lower()
+    string2 = string2.lower()
+    
+    string1 = sorted(string1)
+    string2 = sorted(string2)
+
+    if string1 == string2:
+        return True
+    else:
+        return False
+
+print(is_anagram(string1, string2))
