34c4598b6e7757b0d6fee709a69e94fa1ec0947b
Vladimír Zdražil <mail@vzdrazil.com>
2016-11-08 14:49:39 +0100

    Implement an algorithm to determine if a string has all unique characters

---
+++ data-structures/01-01.py
@@ -0,0 +1,15 @@
+# Implement an algorithm to determine if a string has all unique characters. What if you can not use additional data structures?
+
+string = 'Lorem ipsum'
+
+def check_unique(input):
+    char_set = [0 for x in range(256)]
+    for ind, val in enumerate(input):
+        string_value = ord(val)
+        char_set[string_value] += 1
+        if char_set[string_value] > 1:
+            return False
+    return True
+
+result = check_unique(string)
+print(result)
