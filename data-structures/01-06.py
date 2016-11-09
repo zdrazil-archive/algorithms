88adc2322d84565ab8d840f19255287564c6389c
Vladimír Zdražil <mail@vzdrazil.com>
2016-11-09 13:10:36 +0100

    Rotate image in place, matrix zero

--- data-structures/01-06.py
+++ data-structures/01-06.py
@@ -1,8 +1,10 @@
 # Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degress.
+# Do it in place.
+
 from os import urandom
 from copy import deepcopy
 
-def generate_matrix(N, output = 'num'):
+def generate_matrix(N = 3, output = 'num'):
     if output == 'byte':
         matrix = [[urandom(4) for i in range(N)] for i in range(N)]
     else:
@@ -19,11 +21,35 @@ def rotate_matrix(matrix):
             new_matrix[ind_col][n - 1 - ind_row] = val_col
     return new_matrix
 
-matrix = generate_matrix(5)
+def  (matrix):
+    n = len(matrix)
+    for layer in range(0, int((n / 2) + 1)):
+        first = layer
+        last = n - 1 - layer
+        for i in range(first, last):
+            top = matrix[first][i]
+            offset = i - first
+            # bottom left -> top left
+            matrix[first][i] = matrix[last - offset][first]
+            # bottom right -> bottom left
+            matrix[last - offset][first] = matrix[last][last - offset]
+            # top right -> bottom right
+            matrix[last][last - offset] = matrix[i][last]
+            # top left -> top right
+            matrix[i][last] = top
+    return matrix
+    
+
+matrix = generate_matrix()
 print(matrix)
 rotated_matrix = rotate_matrix(matrix)
 print(rotated_matrix)
 
 matrix = generate_matrix(5, 'byte')
 rotated_matrix= rotate_matrix(matrix)
+print(rotated_matrix)
+
+matrix = generate_matrix()
+print(matrix)
+rotated_matrix = rotate_matrix_in_place(matrix)
 print(rotated_matrix)
---
+++ data-structures/01-07.py
@@ -0,0 +1,17 @@
+# Write an alogrithm such that if an element in an MxN matrix 0, its entire row and column is set to 0
+import copy
+matrix = [[1, 0, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+
+def set_zero(matrix):
+    new_matrix = copy.deepcopy(matrix)
+    for ind_row, val_row in enumerate(matrix):
+        for ind_col, val_col in enumerate(val_row):
+            print(ind_col)
+            if val_col == 0:
+                for ind_change, val_change in enumerate(matrix[ind_row]):
+                    new_matrix[ind_row][ind_change] = 0
+                for ind_change, val_change in enumerate(matrix):
+                    new_matrix[ind_change][ind_col] = 0
+    return(new_matrix)
+
+print(set_zero(matrix))
